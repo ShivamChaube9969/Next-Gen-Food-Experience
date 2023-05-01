750671736afa717a50c39222dcf944d08a7f3aae
Shivam Chaube <shivamkchaube@gmail.com>
2023-05-01 21:38:25 +0530

    Started working on Recipe Logic & datasets for same

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def chat(inp, message_history, role="user"):
     )
 
     # Grab just the text from the API completion response
-    reply_content = completion.choices[0].message
+    reply_content = completion.choices[0].message.content
 
     # Append the generated response to the message history
     message_history.append({"role": "assistant", "content": f"{reply_content}"})
@@ -37,20 +37,25 @@ def chat(inp, message_history, role="user"):
 def index():
 
     # Setting the context
+    # session['message_history'] = [{"role": "user", "content": """
+    # You are OrderBot, an automated service to collect orders for a restaurant. You will provide a list of 20 ingredients that the user will choose from.
+    # When you present the ingredients, present the ingredients like "1: Rice" "2: Noodles" ...etc."""},{"role": "assistant", "content": """Greetings, How can I help you today?"""}]
+
     session['message_history'] = [{"role": "user", "content": """
-    You are OrderBot, an automated service to collect orders for a restaurant. You will provide a list of 20 ingredients that the user will choose from.
-    When you present the ingredients, present the ingredients with a greeting message and start immediately with the ingredients, no further commentary, and then ingredients like "1:" "2:" ...etc. If you understand, say, OK, and begin when I say "begin." """},{"role": "assistant", "content": """
-    OK, I understand. Begin when you're ready."""}]
+    You are OrderBot, an automated service to collect orders for a restaurant. You will provide a list of 10 recipes based on the ingredients that the user is interested to try today.
+    When you present the recipes, present the recpipes like "1: Recipe 1" "2: Recipe 2" ...etc."""},{"role": "assistant", "content": """Greetings, How can I help you today?"""}, {'role':"assistant", 'content':"Let me know which ingredients would you like to try today?"}]
 
     # Retrieve the message history from the session
     message_history = session['message_history']
 
     # Generate a chat response with an initial message ("Begin")
-    reply_content, message_history = chat("Begin", message_history)
-    print(reply_content.content)
-
+    # reply_content, message_history = chat("Can you provide the list of ingredients?", message_history)
+    # reply_content = str(reply_content)
+    # start_index = reply_content.index('1:')
+    # reply_content = reply_content[start_index:]
+    reply_content = "Hello, I am your AI Food assistant. Let me know which ingredients would you like to try today?"
     # Render the index.html file and pass in the message history
-    return render_template("index.html", message=reply_content.content)
+    return render_template("index.html", message=reply_content)
 
 
 # Define the /api route to handle POST requests
@@ -66,6 +71,7 @@ def api():
     reply_content, message_history = chat(message, message_history)
     
     if reply_content!=None:
+        print(reply_content)
         return reply_content
 
     else :
